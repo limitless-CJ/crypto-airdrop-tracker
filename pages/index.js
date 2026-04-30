d372b09ef23520a35c4c2636a88f8d523b0cf3f2
limitless-CJ <asakpabright7@gmail.com>
2026-04-30 16:03:03 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Dashboard from "../components/Dashboard";
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
+      <h1>Smart Crypto Airdrop Tracker</h1>
+      <Dashboard />
+    </div>
+  );
+}
